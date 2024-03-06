f01c48ef948b34b906a4a3da4d24cc8da3b98962
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-03-06 13:23:09 +0545

    Custom CRUD functions implemented

--- Authentication/auth_app/views.py
+++ Authentication/auth_app/views.py
@@ -16,6 +16,47 @@ class RegisterViewSet(viewsets.ModelViewSet):
     queryset=UserData.objects.all()
     serializer_class=UserSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        response_data={
+            'message':" object created successfully",
+            'data':serializer.data
+        }
+        return Response(response_data, status=201)
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            'message': "Object retrieved successfully",
+            'data': serializer.data
+        })
+
+    
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({
+            'message': "Object updated successfully",
+            'data': serializer.data
+        })
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response ({
+             'message': "Object updated successfully"
+    
+
+        })
+    
+
 
     # def obtain_token_pair(self,request):
     #     view=TokenObtainPairView.as_view()
