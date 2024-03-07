0d842a80d03a0ed8e59d3b35562d9501b5677a23
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-03-07 14:14:19 +0545

    update userprofile serializer and update dictionary representations func

--- Authentication/UserProfile/serializer.py
+++ Authentication/UserProfile/serializer.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from .models import UserProfile
+from auth_app.serializer import UserSerializer
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    # user=UserSerializer()
     class Meta:
         model = UserProfile
-        fields = '__all__'
-        # fields = ['first_name', 'last_name', 'phone_number', 'age', 'gender']
+        fields = ['first_name', 'last_name', 'phone_number', 'age', 'gender']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        user_data = instance.user  # Access the related UserData instance
+        representation['email'] = user_data.email
+        representation['name'] = user_data.name
+        representation['password'] = user_data.password  # Note: This might not be ideal to expose in the response
+        # Add more user fields as needed
+        return representation
--- Authentication/UserProfile/views.py
+++ Authentication/UserProfile/views.py
@@ -4,13 +4,62 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import UserProfile
 from .serializer import UserProfileSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
-    # permission_classes = [IsAuthenticated]
+ 
+ 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        response_data={
+            'message':" object created successfully",
+            'data':serializer.data
+        }
+        return Response(response_data, status=201)
     
-    # def get_queryset(self):
-    #     # Return only the profile of the logged-in user
-    #     return UserProfile.objects.filter(user=self.request.user)
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page=self.paginate_queryset(queryset)
+        if page is not None:
+            
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response({
+                 'message':"Users - lists",
+                # 'message': "paginated tag-list",
+                'data': serializer.data
+            })
+        
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            'message': "Object retrieved successfully",
+            'data': serializer.data
+        })
+
+    
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({
+            'message': "Object updated successfully",
+            'data': serializer.data
+        })
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response ({
+             'message': "Object updated successfully"
+    
+
+        })
