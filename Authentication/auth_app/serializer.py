be6283c59f28806f02727a27b5014ef5e9785589
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-03-07 11:38:29 +0545

    update userProfile serialzier for object validation

--- Authentication/auth_app/serializer.py
+++ Authentication/auth_app/serializer.py
@@ -1,21 +1,40 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from .models import UserData
+from .models import UserData,UserProfile
 
-class UserSerializer(serializers.ModelSerializer):
+
+from rest_framework import serializers
+from .models import UserData, UserProfile
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserProfile
+        fields = ('first_name', 'last_name', 'phone_number', 'age', 'gender')
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    
+    profile = UserProfileSerializer(required=False)
 
     class Meta:
         model = UserData
-        fields = ["id", "email", "name", "password"]
+        fields = ('email', 'password', 'profile')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = UserData.objects.create(email=validated_data['email'],
-                                       name=validated_data['name']
-                                         )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user 
-    
+        profile_data = validated_data.pop('profile', None)
+        user = UserData.objects.create_user(**validated_data)
+        if profile_data:
+            UserProfile.objects.create(
+                user=user,
+                first_name=profile_data.get('first_name', ''),
+                last_name=profile_data.get('last_name', ''),
+                phone_number=profile_data.get('phone_number', ''),
+                age=profile_data.get('age', 0),
+                gender=profile_data.get('gender', '')
+            )
+        return user
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
