3739af141ff9990b7e29ddb7fd1d8c0bc3e7ec41
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-03-11 15:38:20 +0545

    code clean in views and serializers user-authentications

--- Authentication/UserProfile/serializer.py
+++ Authentication/UserProfile/serializer.py
@@ -8,27 +8,14 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = UserProfile
         fields = ['user','first_name', 'last_name', 'phone_number', 'age', 'gender']
 
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     user_data = instance.user  # Access the related UserData instance
-    #     representation['email'] = user_data.email
-    #     representation['name'] = user_data.name
-    #     representation['password'] = user_data.password 
-
-    #     return representation
-    
+ 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         authentication_data = {
             'id':instance.user.generated_uuid,
             'name': instance.user.name  ,
             'email': instance.user.email  ,
-            'password': instance.user.password  
-
-
-
-
-              
+            'password': instance.user.password
         }
         representation['users']=authentication_data
         return representation
